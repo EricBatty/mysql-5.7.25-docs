71cffe852354e22b8908304bc3486fd9c22f91c1
Jinxiao Zhang <zhangjinxiao@zhiweidata.com>
2019-04-26 14:17:02 +0800

    ${$1}

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+build() {
+	echo "Begin build"
+	gitbook build
+}
+
+commit() {
+	echo "添加本次提交注释"
+	git add .
+	git commit -m  '${$1}'
+}
+
+push() {
+	echo "上传代码"
+	git push 
+}
+main() {
+	build
+	commit
+	push
+}
+main
